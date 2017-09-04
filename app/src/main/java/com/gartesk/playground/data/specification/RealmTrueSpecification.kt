2f96970ba0322768ad27247f2a61b48a9a7ae573
Kirill Garbuzov <kirill.e.garbuzov@gmail.com>
2017-09-04 21:44:53 +0700

    Code formatting cleanup

--- app/src/main/java/com/gartesk/playground/data/specification/RealmTrueSpecification.kt
+++ app/src/main/java/com/gartesk/playground/data/specification/RealmTrueSpecification.kt
@@ -3,7 +3,7 @@ package com.gartesk.playground.data.specification
 import com.gartesk.playground.data.entity.DataItem
 import io.realm.RealmQuery
 
-class RealmTrueSpecification() : RealmCompositeSpecification<DataItem>() {
+class RealmTrueSpecification : RealmCompositeSpecification<DataItem>() {
 
     override fun apply(query: RealmQuery<DataItem>): RealmQuery<DataItem> = query
 }
